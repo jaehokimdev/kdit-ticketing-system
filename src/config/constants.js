5c5ee690d22b31e8ea0b763a36af74ea17b529a7
Jaeho Kim <101899896+jaehokimdev@users.noreply.github.com>
2023-04-19 23:19:06 -0600

    chagned backend address

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,2 +1,2 @@
-export const baseUrl = "http://132.145.105.75:8000/";
-// export const baseUrl = "http://127.0.0.1:8000/";
+// export const baseUrl = "http://132.145.105.75:8000/";
+export const baseUrl = "http://127.0.0.1:8000/";
